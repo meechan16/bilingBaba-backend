a45ce9199f49fd6a40ca4be14a050637b3c2b803
Harsh Agnihotri <harshagnihotri90@gmail.com>
2024-03-04 03:34:57 +0530

    backend till now

---
+++ app.py
@@ -0,0 +1,210 @@
+from flask import Flask, jsonify, Response, request, send_file
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+from firebase_admin.exceptions import FirebaseError
+from flask_cors import CORS
+# from flask_mail import Mail
+from dotenv import load_dotenv, dotenv_values
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+
+try:
+    cred = credentials.Certificate("auth_key.json")
+except Exception as e:
+    print(e)
+    print("failed to get firebasse auth json from root dir")
+    cred = credentials.Certificate('backend/auth_key.json')
+
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# user data schema
+
+sample_data = {
+    "name": None,
+    "email": None,
+    "todo_list": {
+        "task 1": "done",
+        "task 2": "pending",
+        "task 3": "pending",
+        "task 4": "done",
+        "task 5": "done",
+    },
+    "sales": [],
+    "total_sales": 0,
+    "total_purchase": 0,
+    "total_profit": 0,
+    "to_Collect": 0,
+    "to_pay": 0,
+    "expense": 0,
+    "purchase": 0,
+    "cash_in_hand": 0
+}
+
+
+def check_user_exists(uid):
+    try:
+        user = auth.get_user(uid)
+        print(user)
+        return True
+    except FirebaseError as e:
+        if e.code == 'user-not-found':
+            print("User does not exist")
+            return False
+        else:
+            # Handle other exceptions
+            print("Error occurred:", e)
+            return False
+
+
+def write_to_firestore(custom_id, data, collection="items"):
+    # Add a document with a custom ID to the "items" collection
+    doc_ref = db.collection(collection).document(custom_id)
+    doc_ref.set(data)
+    print("Document '{}' written to Firestore.".format(custom_id))
+
+
+def read_from_firestore(custom_id, collection="items"):
+    # Retrieve a document from the "items" collection
+    doc_ref = db.collection(collection).document(custom_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        print("Document data:", doc.to_dict())
+    else:
+        print("No such document!")
+    return doc.to_dict()
+
+
+def add_Sale(uid, data):
+    prev_data = read_from_firestore(uid, "sales")
+    # prev_data = sample_data
+
+    prev_data["sales"].append(data)
+    print(data)
+    prev_data["total_sales"] += int(data['total'])
+
+    write_to_firestore(uid, prev_data, "sales")
+
+
+# ROOT URLS
+@app.route("/", methods=['GET'])
+def index():
+    return jsonify({"status": "200 - working", "descripttion": "welcome to coachpointai API"})
+
+
+@app.route('/addsales', methods=['POST'])
+def add_items():
+    auth_header = request.headers.get('Authorization')
+    print("Authorization Header:", auth_header)
+    # if not check_user_exists(auth_header):
+    #     return jsonify({"status": "fail", "Description": "user doesnt exist"}), 200
+
+    file_path = request.get_json()
+    print(file_path)
+    add_Sale(auth_header, file_path)
+    return jsonify({"status": "success"}), 200
+
+
+@app.route('/update_todo', methods=['POST'])
+def todo():
+    auth_header = request.headers.get('Authorization')
+    print("Authorization Header:", auth_header)
+    # if not check_user_exists(auth_header):
+    #     return jsonify({"status": "fail", "Description": "user doesnt exist"}), 200
+    reqdata = request.get_json()
+    print(reqdata)
+    write_to_firestore(auth_header, reqdata, "sales")
+    return jsonify({"status": "success"}), 200
+
+
+@app.route('/addsalesAndGetPdf', methods=['POST'])
+def add_items_and_get_pdf():
+    auth_header = request.headers.get('Authorization')
+    print("Authorization Header:", auth_header)
+    # if not check_user_exists(auth_header):
+    #     return jsonify({"status": "fail", "Description": "user doesnt exist"}), 200
+
+    data = request.get_json()
+    print(data)
+    write_to_firestore(
+        data=data, custom_id=auth_header, collection="items")
+
+    from bill_pdf_maker import create_tax_invoice_pdf
+    buffer = create_tax_invoice_pdf(data)
+    return send_file(buffer, mimetype='application/pdf')
+
+
+@app.route('/get_user', methods=['GET'])
+def get_items():
+    auth_header = request.headers.get('Authorization')
+    print("Authorization Header:", auth_header)
+    # if not check_user_exists(auth_header):
+    #     return jsonify({"status": "fail", "Description": "user doesnt exist"}), 200
+
+    res = read_from_firestore(custom_id=auth_header, collection="sales")
+    return jsonify({"status": "success", "data": res}), 200
+
+
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+# app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+# app.config['MAIL_USE_TLS'] = True   # Set it to False if you're using SSL
+# app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
+
+# mail = Mail(app)
+
+# from app.routes.user_routes import USER_BLUEPRINT
+# from app.routes.data_routes import DATA_BP
+
+# app.register_blueprint(USER_BLUEPRINT)
+# app.register_blueprint(DATA_BP)
+
+
+# @app.route('/contact_us', methods=['POST'])
+# def Support_mail():
+#     data = request.get_json()
+
+#     subject = str(data.get('email'))
+#     body = data.get('body')
+#     email = 'karan@coachpoints.ai'
+
+#     from app.utils.Others import send_email
+#     # user_mail_body = "Thank you for reaching out to us. \nWe genuinely appreciate your inquiry, and our dedicated team is committed to providing you with the best possible assistance. \nYour query is important to us, and we will respond as soon as possible.\n In the meantime, if you have any further questions or require immediate assistance,\n please don't hesitate to get in touch. \nWe look forward to helping you with your needs.\n\nCoachpoints.ai"
+#     user_mail_body = '''
+# Thank you for reaching out to us.
+# We appreciate your inquiry, and our dedicated team will get back to you asap.
+
+# In the meantime, if you have any further questions or require immediate assistance, please reply to this email with your email id and query.
+
+# Thanks,
+# Team CoachPoints AI
+#     '''
+#     User_email = send_email(email=subject, subject="Querry Submission - Team Coachpoint",body= user_mail_body)
+#     system_email = send_email(email, "Querry from "+subject, body)
+#     if User_email and system_email:
+#         return jsonify({"message": "Email sent successfully!"}), 200
+#     else:
+#         return jsonify({"error": "Failed to send email."}), 500
+
+
+# from app.utils.mongoDb import ADMIN
+# @app.route('/admin', methods=['GET'])
+# def admin():
+#     Id = request.args.get('id')
+#     if Id == "cheesecake":
+#         data = ADMIN.get_data()
+#         print(type(data))
+#         if '_id' in data:
+#             del data['_id']
+#         return format_dictionary({"Status":True,"data":data}), 200
+#     else:
+#         return jsonify({"status":"False","Desc":"prohabbited"}),200
+
+if __name__ == '__main__':
+    app.run(port=9000, debug=True)
---
+++ bill_pdf_maker.py
@@ -0,0 +1,144 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from io import BytesIO
+
+
+'''
+{
+
+    // 
+    date
+    [product name, quantity, unit price, total price]
+    sub total without tax
+    tax (1 model)
+
+
+
+
+    // generated 
+    invoice number    
+    }
+'''
+
+predata = {
+    "name": "Billing Baba",
+    "email": "support@billingbaba.com",
+    "phone": "+91 100XXXXXXX"
+}
+
+
+# def create_tax_invoice_pdf(filename):
+def create_tax_invoice_pdf(data):
+    # Create canvas
+    buffer = BytesIO()
+
+    # Create canvas
+    c = canvas.Canvas(buffer, pagesize=letter)
+    # c = canvas.Canvas(filename, pagesize=letter)
+
+    # Set up fonts and styles
+    c.setFont("Helvetica", 10)  # Set font and size for business details
+    c.setFillColorRGB(0, 0, 0)  # Set color to black
+
+    # Business details
+    c.drawString(50, 750, predata['name'])
+    c.drawString(50, 735, "Phone: "+predata["phone"])
+    c.drawString(50, 720, "Email: "+predata["email"])
+
+    # Logo
+    logo_path = "backend/logo.jpg"  # Replace with the path to your logo image
+    # Adjust coordinates and size as needed
+    c.drawImage(logo_path, x=450, y=720, width=100, height=50)
+
+    # Draw line
+    c.setStrokeColorRGB(0, 0, 0)  # Set line color to black
+    c.line(50, 710, 550, 710)
+
+    # Heading
+    c.setFont("Helvetica-Bold", 16)
+    c.setFillColorRGB(0, 0, 0)  # Reset color to black
+    c.drawCentredString(300, 680, "Tax Invoice")
+
+    # Purple line
+    c.setStrokeColorRGB(0.5, 0, 0.5)  # Set line color to purple
+    c.setLineWidth(3)  # Set line thickness
+    c.line(50, 660, 550, 660)
+
+    # Invoice details
+    c.setFont("Helvetica-Bold", 12)
+    c.setFillColorRGB(0, 0, 0)  # Reset color to black
+    c.drawString(50, 630, "Invoice Details")
+    c.setFont("Helvetica", 10)
+    c.drawString(50, 615, "Invoice No.: "+str(data['invoice_number']))
+    c.drawString(50, 600, "Date: "+data["invoice_date"])
+
+    c.drawString(
+        50, 585, "Description: "+data["description"])
+
+    # Draw table
+    table_y = 540
+    c.setFillColorRGB(0.5, 0, 0.5)  # Set fill color to purple
+    c.rect(50, table_y, 500, 20, fill=1)  # Filled rectangle for header row
+    c.setFillColorRGB(1, 1, 1)  # Set fill color to white for text
+    c.setFont("Helvetica-Bold", 10)
+    c.drawString(55, table_y + 5, "Description")
+    c.drawString(255, table_y + 5, "Quantity")
+    c.drawString(305, table_y + 5, "Unit Price")
+    c.drawString(405, table_y + 5, "Total Price")
+
+    # Dummy data for table
+    c.setFillColorRGB(0, 0, 0)  # Reset fill color to black
+    y = table_y - 20
+    # for _ in range(7):
+    #     c.drawString(55, y, "Product Name")
+    #     c.drawString(255, y, "2")
+    #     c.drawString(305, y, "$25.00")
+    #     c.drawString(405, y, "$50.00")
+    #     y -= 20
+    for i in data['items']:
+        c.drawString(55, y, str(i["item"]))
+        c.drawString(255, y, str(i["qty"]))
+        c.drawString(305, y, str(i["price_per_unit"]))
+        c.drawString(405, y, str(i["amount"]))
+        y -= 20
+
+    # Adding space after the table
+    content_y = y - 40
+
+    # Additional text
+    invoice_amount_text = "Total Amount: "+str(data["total"])
+    c.setFont("Helvetica-Bold", 10)
+    c.drawString(50, content_y, "Invoice Summary:")
+    c.setFont("Helvetica", 10)
+    c.drawString(50, content_y - 15, invoice_amount_text)
+
+    # Values on the right side in table format
+    data = [
+        ("Sub Total", "₹ "+str(data['total'] - data['total_tax'])),
+        ("Tax (0%)", "₹ "+str(data['total_tax'])),
+        ("Total", "₹ "+str(data['total'])),
+        ("Paid", "₹ 0.00"),
+        ("Balance Due", "₹ "+str(data['total']))
+    ]
+    y_offset = 0
+    for item, value in data:
+        c.drawString(300, content_y + y_offset, item)
+        c.drawRightString(500, content_y + y_offset, value)
+        y_offset -= 15
+
+    # Signature and authorization
+    c.line(50, content_y - 130, 300, content_y - 130)  # Line for signature
+    c.drawString(50, content_y - 150, "Authorized Signature")
+
+    # Save the PDF
+    c.save()
+
+    buffer.seek(0)
+
+    # Return the PDF file as a response
+    return buffer
+    # return send_file(buffer, as_attachment=True, attachment_filename='styled_tax_invoice.pdf', mimetype='application/pdf')
+
+
+# Create tax invoice PDF
+# create_tax_invoice_pdf("styled_tax_invoice.pdf")
